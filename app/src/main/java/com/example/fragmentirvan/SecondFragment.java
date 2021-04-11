2a586048325f213dd5e40bf5908de4c60d15f4c4
irvanican <irvan.chndr.01@gmail.com>
2021-04-11 22:27:46 +0700

    fragmentirvan

--- app/src/main/java/com/example/fragmentirvan/SecondFragment.java
+++ app/src/main/java/com/example/fragmentirvan/SecondFragment.java
@@ -1,4 +1,19 @@
 package com.example.fragmentirvan;
 
-public class SecondFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanteState){
+        return inflater.inflate(R.layout.fragment_second, container, false);
+
+    }
 }
